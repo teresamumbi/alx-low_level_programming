cbe1c49bd8f5132c737cf7c16f18edfb8374d786
teresamumbi <teresamumbi@gmail.com>
2022-03-17 11:12:55 +0000

    1 alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ */
+
+void print_alphabet(void)
+
+{
+
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	putchar('\n');
+
+}
